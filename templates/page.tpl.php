003b66813743cbffd520d60d34e352053e50dbeb
root <root@i7releaselocaldev.i7release.dgicloud.com>
2016-10-27 06:31:45 -0700

    client change for logo href

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -76,7 +76,7 @@
   <header id="header" role="banner">
 
     <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+      <a href="https://www.mlml.calstate.edu" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
     <?php endif; ?>
     <?php if ($site_name || $site_slogan): ?>
       <hgroup id="name-and-slogan">
